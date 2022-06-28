1f5ea563a7045a7eac2d6bd017e04372910225c8
Kit Schwarz <kschwarz@battuta.berkeley.edu>
2022-06-28 14:23:36 -0700

    black

--- dscim/preprocessing/input_damages/coastal_inputs.py
+++ dscim/preprocessing/input_damages/coastal_inputs.py
@@ -2,13 +2,14 @@ import xarray as xr
 
 print("testing message: version jun 25")
 
+
 def coastal_inputs(
     vsl_valuation,
     adapt_type,
-    path = "/shares/gcp/integration/float32/input_data_histclim/coastal_data/",
+    path="/shares/gcp/integration/float32/input_data_histclim/coastal_data/",
     v=0.21,
 ):
-    
+
     d = xr.open_zarr(f"{input_path}/coastal_damages_v{v}.zarr")
 
     d.sel(adapt_type=adapt_type, vsl_valuation=vsl_valuation, drop=True).to_zarr(
