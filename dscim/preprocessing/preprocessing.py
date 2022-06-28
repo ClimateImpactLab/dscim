2f4df63d7052cea20a71be81f8c59624afba5d90
Kit Schwarz <kschwarz@battuta.berkeley.edu>
2022-06-28 14:02:04 -0700

    finishing reduce_damages function; tested succesfully

--- dscim/preprocessing/preprocessing.py
+++ dscim/preprocessing/preprocessing.py
@@ -1,6 +1,7 @@
 from math import ceil
 from pathlib import Path
 import os, sys
+
 USER = os.getenv("USER")
 import dask
 import dask.array as da
@@ -11,14 +12,27 @@ from dask.distributed import Client, progress
 from dscim.utils.functions import ce_func, mean_func
 import yaml, time, argparse
 
-def ce_from_chunk(chunk, filepath, ce_type, bottom_code, histclim, delta, recipe, eta):
+
+def ce_from_chunk(
+    chunk,
+    filepath,
+    ce_type,
+    bottom_code,
+    histclim,
+    delta,
+    recipe,
+    eta,
+    zero,
+    socioec,
+    ce_batch_coords,
+):
 
     year = chunk.year.values
     ssp = chunk.ssp.values
     model = chunk.model.values
 
     gdppc = (
-        xr.open_zarr(ECON_ZARR, chunks=None)
+        xr.open_zarr(socioec, chunks=None)
         .sel(
             year=year, ssp=ssp, model=model, region=ce_batch_coords["region"], drop=True
         )
@@ -26,7 +40,7 @@ def ce_from_chunk(chunk, filepath, ce_type, bottom_code, histclim, delta, recipe
     )
 
     if ce_type == "ce_no_cc":
-        if args.zero == True:
+        if zero == True:
             chunk[histclim] = xr.where(chunk[histclim] == 0, 0, 0)
         calculation = gdppc + chunk[histclim].mean("batch") - chunk[histclim]
     elif ce_type == "ce_cc":
@@ -54,23 +68,22 @@ def ce_from_chunk(chunk, filepath, ce_type, bottom_code, histclim, delta, recipe
 
     return result
 
-def aggregate_damages(
+
+def reduce_damages(
     recipe,
     eta,
     rho,
     sector,
-    ce_type,
+    reduction,
     config,
-    zero,
-    outpath
-    
-    )
+    outpath,
+    zero=False,
+):
 
     bottom_coding_gdppc = 39.39265060424805
 
-    from dask.distributed import Client
-
-    if __name__ == "__ce_calculation__":
+    if __name__ == "__reduce_damages__":
+        print("Starting client...")
 
         client = Client(n_workers=35, memory_limit="9G", threads_per_worker=1)
 
@@ -78,16 +91,16 @@ def aggregate_damages(
         loaded_config = yaml.safe_load(stream)
         params = loaded_config["sectors"][sector]
 
-    ZARR = Path(params["sector_path"])
+    damages = Path(params["sector_path"])
     histclim = params["histclim"]
     delta = params["delta"]
 
-    ECON_ZARR = Path(
+    socioec = Path(
         "/shares/gcp/integration/float32/dscim_input_data/econvars/zarrs/integration-econ-bc39.zarr"
     )
 
-    with xr.open_zarr(ZARR, chunks=None)[histclim] as ds:
-        with xr.open_zarr(ECON_ZARR, chunks=None) as gdppc:
+    with xr.open_zarr(damages, chunks=None)[histclim] as ds:
+        with xr.open_zarr(socioec, chunks=None) as gdppc:
 
             ce_batch_dims = [i for i in gdppc.dims] + [
                 i for i in ds.dims if i not in gdppc.dims and i != "batch"
@@ -97,8 +110,7 @@ def aggregate_damages(
                 i for i in gdppc.region.values if i in ce_batch_coords["region"]
             ]
             ce_shapes = [len(ce_batch_coords[c]) for c in ce_batch_dims]
-            ce_chunks = [xr.open_zarr(ZARR).chunks[c][0] for c in ce_batch_dims]
-            print(ce_chunks)
+            ce_chunks = [xr.open_zarr(damages).chunks[c][0] for c in ce_batch_dims]
 
     template = xr.DataArray(
         da.empty(ce_shapes, chunks=ce_chunks),
@@ -108,42 +120,40 @@ def aggregate_damages(
 
     # convert string eta to float
     eta = float(eta)
-    print("Eta is: ", eta)
-    print(f"{config}")
-    print(f"{sector}")
-    print(outpath)
 
-
-    other = xr.open_zarr(ZARR)
+    other = xr.open_zarr(damages)
 
     out = other.map_blocks(
         ce_from_chunk,
         kwargs=dict(
-            filepath=ZARR,
-            ce_type=ce_type,
+            filepath=damages,
+            ce_type=reduction,
             bottom_code=bottom_coding_gdppc,
             histclim=histclim,
             delta=delta,
             eta=eta,
             recipe=recipe,
+            zero=zero,
+            socioec=socioec,
+            ce_batch_coords=ce_batch_coords,
         ),
         template=template,
     )
 
-    out = out.astype(np.float32).rename(ce_type).to_dataset()
+    out = out.astype(np.float32).rename(reduction).to_dataset()
 
     out.attrs["eta"] = eta
     out.attrs["bottom code"] = bottom_coding_gdppc
     out.attrs["histclim=0"] = zero
-    out.attrs["filepath"] = str(ZARR)
+    out.attrs["filepath"] = str(damages)
 
     out.to_zarr(
-        f"{outpath}/{recipe}_{ce}_eta{eta}.zarr",
+        f"{outpath}/{recipe}_{reduction}_eta{eta}.zarr",
         consolidated=True,
         mode="w",
     )
 
-    
+
 def reformat_climate_files():
     from dscim.preprocessing.climate.reformat import (
         convert_old_to_newformat_AR,
