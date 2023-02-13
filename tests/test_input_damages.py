4a69ee68917ddabf47d91b6af4472541bc688a07
davidrzhdu <davidrzhdu@uchicago.edu>
2023-02-13 08:50:54 -0800

    Add pytests for processing energy input damages

--- tests/test_input_damages.py
+++ tests/test_input_damages.py
@@ -12,6 +12,12 @@ from dscim.preprocessing.input_damages import (
     concatenate_labor_damages,
     calculate_labor_batch_damages,
     calculate_labor_damages,
+    read_energy_files,
+    read_energy_files_parallel,
+    calculate_energy_impacts,
+    concatenate_energy_damages,
+    calculate_energy_batch_damages,
+    calculate_energy_damages,
 )
 
 logger = logging.getLogger(__name__)
@@ -319,3 +325,415 @@ def test_calculate_labor_damages(
         )
 
         xr.testing.assert_equal(ds_out_expected, ds_out_actual)
+
+
+@pytest.fixture
+def energy_in_csv(tmp_path):
+    rcp = ["rcp45", "rcp85"]
+    gcm = [
+        "ACCESS1-0",
+        "CCSM4",
+        "GFDL-CM3",
+        "IPSL-CM5A-LR",
+        "MIROC-ESM-CHEM",
+        "bcc-csm1-1",
+        "CESM1-BGC",
+        "GFDL-ESM2G",
+        "IPSL-CM5A-MR",
+        "MPI-ESM-LR",
+        "BNU-ESM",
+        "CNRM-CM5",
+        "GFDL-ESM2M",
+    ]
+    model = ["low", "high"]
+    ssp = ["SSP3"]
+    batch = ["batch" + str(i) for i in range(0, 15)]
+
+    for var in ["delta", "histclim"]:
+        for r in rcp:
+            for g in gcm:
+                for m in model:
+                    for s in ssp:
+                        for b in batch:
+                            d = os.path.join(tmp_path, "energy_in_csv", b, r, g, m, s)
+                            if not os.path.exists(d):
+                                os.makedirs(d)
+                            infile = os.path.join(
+                                d, f"TINV_clim_integration_total_energy_{var}.csv"
+                            )
+                            df = {
+                                "region": [
+                                    "ZWE.test_region",
+                                    "ZWE.test_region",
+                                    "USA.test_region",
+                                    "USA.test_region",
+                                ],
+                                "year": [2010, 2099, 2010, 2099],
+                                "rebased": [2, 2, 2, 2],
+                            }
+                            df_in = pd.DataFrame(data=df)
+                            df_in.to_csv(infile, index=False)
+
+
+def test_read_energy_files(
+    tmp_path,
+    energy_in_csv,
+):
+    read_energy_files(
+        df=_parse_projection_filesys(
+            input_path=os.path.join(tmp_path, "energy_in_csv")
+        ),
+        seed="TINV_clim_integration_total_energy_delta",
+    )
+
+    rcp = ["rcp45", "rcp85"]
+    gcm = [
+        "ACCESS1-0",
+        "CCSM4",
+        "GFDL-CM3",
+        "IPSL-CM5A-LR",
+        "MIROC-ESM-CHEM",
+        "bcc-csm1-1",
+        "CESM1-BGC",
+        "GFDL-ESM2G",
+        "IPSL-CM5A-MR",
+        "MPI-ESM-LR",
+        "BNU-ESM",
+        "CNRM-CM5",
+        "GFDL-ESM2M",
+    ]
+    model = ["low", "high"]
+    ssp = ["SSP3"]
+    batch = ["batch" + str(i) for i in range(0, 15)]
+
+    model_trans = {
+        "low": "IIASA GDP",
+        "high": "OECD Env-Growth",
+    }
+
+    for r in rcp:
+        for g in gcm:
+            for m in model:
+                for s in ssp:
+                    for b in batch:
+                        d = os.path.join(tmp_path, "energy_in_csv", b, r, g, m, s)
+                        d_dir = os.path.join(
+                            d, "TINV_clim_integration_total_energy_delta.nc4"
+                        )
+                        ds_out_actual = xr.open_dataset(d_dir)
+
+                        ds_out_expected = xr.Dataset(
+                            {
+                                "rebased": (
+                                    [
+                                        "batch",
+                                        "rcp",
+                                        "gcm",
+                                        "model",
+                                        "ssp",
+                                        "region",
+                                        "year",
+                                    ],
+                                    np.full((1, 1, 1, 1, 1, 2, 2), 2),
+                                ),
+                            },
+                            coords={
+                                "batch": (["batch"], [b]),
+                                "rcp": (["rcp"], [r]),
+                                "gcm": (["gcm"], [g]),
+                                "model": (["model"], [model_trans[m]]),
+                                "ssp": (["ssp"], [s]),
+                                "region": (
+                                    ["region"],
+                                    ["USA.test_region", "ZWE.test_region"],
+                                ),
+                                "year": (["year"], [2010, 2099]),
+                            },
+                        )
+
+                        xr.testing.assert_equal(ds_out_expected, ds_out_actual)
+
+
+def test_read_energy_files_parallel(
+    tmp_path,
+    energy_in_csv,
+):
+    read_energy_files_parallel(
+        input_path=os.path.join(tmp_path, "energy_in_csv"),
+        save_path="unused",
+        ec_cls="unused",
+        seed="TINV_clim_integration_total_energy_delta",
+    )
+
+    rcp = ["rcp45", "rcp85"]
+    gcm = [
+        "ACCESS1-0",
+        "CCSM4",
+        "GFDL-CM3",
+        "IPSL-CM5A-LR",
+        "MIROC-ESM-CHEM",
+        "bcc-csm1-1",
+        "CESM1-BGC",
+        "GFDL-ESM2G",
+        "IPSL-CM5A-MR",
+        "MPI-ESM-LR",
+        "BNU-ESM",
+        "CNRM-CM5",
+        "GFDL-ESM2M",
+    ]
+    model = ["low", "high"]
+    ssp = ["SSP3"]
+    batch = ["batch" + str(i) for i in range(0, 15)]
+
+    model_trans = {
+        "low": "IIASA GDP",
+        "high": "OECD Env-Growth",
+    }
+
+    for r in rcp:
+        for g in gcm:
+            for m in model:
+                for s in ssp:
+                    for b in batch:
+                        d = os.path.join(tmp_path, "energy_in_csv", b, r, g, m, s)
+                        d_dir = os.path.join(
+                            d, "TINV_clim_integration_total_energy_delta.nc4"
+                        )
+                        ds_out_actual = xr.open_dataset(d_dir)
+
+                        ds_out_expected = xr.Dataset(
+                            {
+                                "rebased": (
+                                    [
+                                        "batch",
+                                        "rcp",
+                                        "gcm",
+                                        "model",
+                                        "ssp",
+                                        "region",
+                                        "year",
+                                    ],
+                                    np.full((1, 1, 1, 1, 1, 2, 2), 2),
+                                ),
+                            },
+                            coords={
+                                "batch": (["batch"], [b]),
+                                "rcp": (["rcp"], [r]),
+                                "gcm": (["gcm"], [g]),
+                                "model": (["model"], [model_trans[m]]),
+                                "ssp": (["ssp"], [s]),
+                                "region": (
+                                    ["region"],
+                                    ["USA.test_region", "ZWE.test_region"],
+                                ),
+                                "year": (["year"], [2010, 2099]),
+                            },
+                        )
+
+                        xr.testing.assert_equal(ds_out_expected, ds_out_actual)
+
+
+@pytest.fixture
+def energy_in_netcdf(tmp_path):
+    model_trans = {
+        "low": "IIASA GDP",
+        "high": "OECD Env-Growth",
+    }
+    rcp = ["rcp45", "rcp85"]
+    gcm = ["ACCESS1-0", "GFDL-CM3"]
+    model = ["low", "high"]
+    ssp = ["SSP3"]
+    batch = ["batch" + str(i) for i in range(0, 15)]
+
+    for var in ["delta", "histclim"]:
+        for r in rcp:
+            for g in gcm:
+                for m in model:
+                    for s in ssp:
+                        for b in batch:
+                            d = os.path.join(
+                                tmp_path, "energy_in_netcdf", b, r, g, m, s
+                            )
+                            if not os.path.exists(d):
+                                os.makedirs(d)
+                            infile = os.path.join(
+                                d, f"TINV_clim_integration_total_energy_{var}.nc4"
+                            )
+
+                            ds_in = xr.Dataset(
+                                {
+                                    "rebased": (
+                                        [
+                                            "batch",
+                                            "rcp",
+                                            "gcm",
+                                            "model",
+                                            "ssp",
+                                            "region",
+                                            "year",
+                                        ],
+                                        np.full((1, 1, 1, 1, 1, 2, 2), 2),
+                                    ),
+                                },
+                                coords={
+                                    "batch": (["batch"], [b]),
+                                    "rcp": (["rcp"], [r]),
+                                    "gcm": (["gcm"], [g]),
+                                    "model": (["model"], [model_trans[m]]),
+                                    "ssp": (["ssp"], [s]),
+                                    "region": (
+                                        ["region"],
+                                        ["USA.test_region", "ZWE.test_region"],
+                                    ),
+                                    "year": (["year"], [2010, 2099]),
+                                },
+                            )
+
+                            ds_in.to_netcdf(infile)
+
+
+def test_calculate_energy_impacts(
+    tmp_path,
+    energy_in_netcdf,
+):
+    ds_out_expected = xr.Dataset(
+        {
+            "histclim_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.full((1, 1, 1, 1, 1, 2, 2), 2),
+            ),
+            "delta_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.full((1, 1, 1, 1, 1, 2, 2), 2),
+            ),
+        },
+        coords={
+            "batch": (["batch"], ["batch6"]),
+            "rcp": (["rcp"], ["rcp45"]),
+            "gcm": (["gcm"], ["ACCESS1-0"]),
+            "model": (["model"], ["IIASA GDP"]),
+            "ssp": (["ssp"], ["SSP3"]),
+            "region": (["region"], ["USA.test_region", "ZWE.test_region"]),
+            "year": (["year"], [2010, 2099]),
+        },
+    )
+
+    d = os.path.join(
+        tmp_path, "energy_in_netcdf", "batch6", "rcp45", "ACCESS1-0", "low", "SSP3"
+    )
+
+    ds_out_actual = calculate_energy_impacts(
+        input_path=d,
+        file_prefix="TINV_clim_integration_total_energy",
+        variable="rebased",
+    )
+    xr.testing.assert_equal(ds_out_expected, ds_out_actual)
+
+
+@pytest.mark.parametrize("out_format", ["return", "save"])
+def test_concatenate_energy_damages(
+    tmp_path,
+    econvars,
+    energy_in_netcdf,
+    out_format,
+):
+    ds_out_expected = xr.Dataset(
+        {
+            "histclim_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.float32(np.full((1, 2, 2, 2, 1, 2, 2), 2 * 1.273526)),
+            ),
+            "delta_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.float32(np.full((1, 2, 2, 2, 1, 2, 2), 2 * 1.273526)),
+            ),
+        },
+        coords={
+            "batch": (["batch"], ["batch9"]),
+            "rcp": (["rcp"], ["rcp45", "rcp85"]),
+            "gcm": (["gcm"], ["ACCESS1-0", "GFDL-CM3"]),
+            "model": (["model"], ["IIASA GDP", "OECD Env-Growth"]),
+            "ssp": (["ssp"], ["SSP3"]),
+            "region": (["region"], ["USA.test_region", "ZWE.test_region"]),
+            "year": (["year"], [2010, 2099]),
+        },
+    )
+
+    ds_out_actual = concatenate_energy_damages(
+        input_path=os.path.join(tmp_path, "energy_in_netcdf"),
+        save_path=tmp_path,
+        ec_cls=econvars,
+    )
+
+    if out_format == "return":
+        xr.testing.assert_equal(ds_out_expected, ds_out_actual)
+
+    elif out_format == "save":
+        xr.testing.assert_equal(
+            ds_out_expected,
+            xr.open_dataset(os.path.join(tmp_path, "rebased_batch9.nc4")),
+        )
+
+
+def test_error_concatenate_energy_damages(
+    caplog,
+    econvars,
+    tmp_path,
+    energy_in_netcdf,
+):
+    os.makedirs(
+        os.path.join(
+            tmp_path, "energy_in_netcdf", "batch6", "rcp45", "CCSM4", "high", "SSP3"
+        )
+    )
+    concatenate_energy_damages(
+        input_path=os.path.join(tmp_path, "energy_in_netcdf"),
+        save_path=tmp_path,
+        ec_cls=econvars,
+    )
+    assert "Error in batchbatch6" in caplog.text
+
+    os.rmdir(
+        os.path.join(
+            tmp_path, "energy_in_netcdf", "batch6", "rcp45", "CCSM4", "high", "SSP3"
+        )
+    )
+
+
+def test_calculate_energy_batch_damages(
+    tmp_path,
+    econvars,
+    energy_in_netcdf,
+):
+    ds_out_expected = xr.Dataset(
+        {
+            "histclim_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.float32(np.full((1, 2, 2, 2, 1, 2, 2), 2 * 1.273526)),
+            ),
+            "delta_rebased": (
+                ["batch", "rcp", "gcm", "model", "ssp", "region", "year"],
+                np.float32(np.full((1, 2, 2, 2, 1, 2, 2), 2 * 1.273526)),
+            ),
+        },
+        coords={
+            "batch": (["batch"], ["batch6"]),
+            "rcp": (["rcp"], ["rcp45", "rcp85"]),
+            "gcm": (["gcm"], ["ACCESS1-0", "GFDL-CM3"]),
+            "model": (["model"], ["IIASA GDP", "OECD Env-Growth"]),
+            "ssp": (["ssp"], ["SSP3"]),
+            "region": (["region"], ["USA.test_region", "ZWE.test_region"]),
+            "year": (["year"], [2010, 2099]),
+        },
+    )
+
+    calculate_energy_batch_damages(
+        batch=6,
+        ec=econvars,
+        input_path=os.path.join(tmp_path, "energy_in_netcdf"),
+        save_path=tmp_path,
+    )
+
+    ds_out_actual = xr.open_zarr(os.path.join(tmp_path, "rebased_batch6.zarr"))
+
+    xr.testing.assert_equal(ds_out_expected, ds_out_actual)
