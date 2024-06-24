6e608f6d2d2029e9d71210a0ebe2fbc38ada6cd3
Jonah Gilbert <jonahmgilbert1@gmail.com>
2024-06-24 11:21:46 -0500

    Add all country scc code

--- src/dscim/menu/risk_aversion.py
+++ src/dscim/menu/risk_aversion.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import xarray as xr
 from dscim.menu.main_recipe import MainRecipe
+import numpy as np
+from dscim.descriptors import cachedproperty
 
 
 class RiskAversionRecipe(MainRecipe):
@@ -38,7 +40,7 @@ class RiskAversionRecipe(MainRecipe):
 
         return ce_array
 
-    @property
+    @cachedproperty
     def calculated_damages(self) -> xr.DataArray:
         """Calculate damages (difference between CEs)"""
         return self.ce_no_cc - self.ce_cc
@@ -61,6 +63,38 @@ class RiskAversionRecipe(MainRecipe):
             )
 
         return df
+    
+    def country_damages_calculation(self, country) -> pd.DataFrame:
+        """Aggregate damages to country level
+
+        Returns
+        --------
+            pd.DataFrame
+        """
+
+        self.logger.info(f"Calculating damages for {country}")
+        dams_collapse = self.calculated_damages * self.collapsed_pop
+        
+        territories = []
+        for ii, row in self.countries_mapping.iterrows():
+            if row['MatchedISO'] == country:
+                territories.append(row['ISO'])
+        self.logger.debug(f"Including territories {territories}")
+        
+        dams_collapse = dams_collapse.sel(
+            region=[i for i in dams_collapse.region.values if any(j in i for j in territories)]
+        )
+        
+        dams_collapse = dams_collapse.sum(dim="region").to_dataframe("damages").reset_index()
+
+        if "gwr" in self.discounting_type:
+            dams_collapse = dams_collapse.assign(
+                ssp=str(list(self.gdp.ssp.values)),
+                model=str(list(self.gdp.model.values)),
+            )
+
+        return dams_collapse
+
 
     def global_consumption_calculation(self, disc_type):
         """Calculate global consumption
@@ -71,7 +105,10 @@ class RiskAversionRecipe(MainRecipe):
         """
 
         if (disc_type == "constant") or ("ramsey" in disc_type):
-            global_cons_no_cc = self.gdp.sum(dim=["region"])
+            if self.geography == 'country':
+                global_cons_no_cc = self.country_econ_vars.gdp
+            else:
+                global_cons_no_cc = self.gdp.sum(dim=["region"])
 
         elif disc_type == "constant_model_collapsed":
             global_cons_no_cc = self.gdp.sum(dim=["region"]).mean(dim=["model"])
